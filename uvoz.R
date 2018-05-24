7564f1a8133d81048c7aa3e260600a48daccb156
marinasirk <mariiina.ms@gmail.com>
2018-05-24 13:04:08 +0200

    Update uvoz.R

--- uvoz.R
+++ uvoz.R
@@ -24,7 +24,7 @@ tabela<-uvozi()
 tabela<-tabela[1:6]
 names(tabela)<-stolpci
 
-#izbrišemo nepopolne vrstice (vrstice z NA)
+#izbriÅ¡emo nepopolne vrstice (vrstice z NA)
 
 delete.na <- function(DF, n=0) {
   DF[rowSums(is.na(DF)) <= n,]
@@ -39,17 +39,41 @@ write.csv(tidy_tabela, "tabela.csv")
 
 # posamezne tabele
 
-izvajalec <- list(tidy_tabela[1])
-#izbrise iste vrstice
-izbrisi <- function(seznam){
-  nov_seznam <- c()
-  for (i in seznam){
-    if (i not in nov_seznam){
-      append(nov_seznam,i)
-    }
-    else{
-    }
+# tabela izvajalec
+
+authors <- c()
+for(author in tidy_tabela$artist){
+  if(!(author %in% authors)){
+    authors=c(authors, author)
   }
 }
+author_id <- c(1:length(authors))
+izvajalec=data.frame(author_id, authors)
 
+# tabela album
 
+naslov_albuma <-c()
+for(album in tidy_tabela$`album name`){
+  if(!(album %in% naslov_albuma)){
+    naslov_albuma=c(naslov_albuma, album)
+  }
+}
+album_id <- c(1:length(naslov_albuma))
+album=data.frame(album_id, naslov_albuma)
+
+# tabela zvrst
+
+ime_zvrsti <- c()
+for(zvrst in tidy_tabela$genre){
+  if(!(zvrst %in% ime_zvrsti)){
+    ime_zvrsti=c(ime_zvrsti, zvrst)
+  }
+}
+zvrst_id <- c(1:length(ime_zvrsti))
+zvrst=data.frame(zvrst_id, ime_zvrsti)
+
+# tabela pesmi
+
+pesem_id <- c(1:length(tidy_tabela$`song name`))
+pesmi <- tidy_tabela[ , c(2,4,6)]
+pesem <- data.frame(pesem_id, pesmi)
